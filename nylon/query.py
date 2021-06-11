4b8e06bcee4d846acaed1c12501596b84c54908a
Palash Shah <35114859+Palashio@users.noreply.github.com>
2021-06-11 14:20:55 -0400

    Update query.py

--- nylon/query.py
+++ nylon/query.py
@@ -7,18 +7,23 @@ class nylonProcessor:
     :param dataset_path is the path to the dataset
     :param save_model: whether the model should be saved in a .sav file
     '''
-    def __init__(self, dataset_path, save_model=False, custom_files = []):
+    def __init__(self, dataset_path, save_model=False, custom_files=[]):
         self.df = dataset_path
         self.json_file = None
         self.y = None
         self.model = None
         self.results = None
         self.custom_files = custom_files
+        self.history = []
     def run(self, json_file_path):
         '''
         Runs the dataset on a json file specification
         :param json_file_path path to json file for
         '''
+
+        if self.model is not None:
+
+
         request_info = {'df': self.df, 'json': json_file_path, 'y': None, 'model': 'None', 'analysis': None, 'custom': self.custom_files}
         pipeline = [
             dataset_initializer,
@@ -35,3 +40,5 @@ class nylonProcessor:
         self.df = request_info['df']
 
         return self
+
+    def store_in_history(self):
